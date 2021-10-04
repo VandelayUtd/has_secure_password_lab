609a754c5468c55c1fcc453e288d943c913732b7
VandelayUtd <drewhadley.guitar@gmail.com>
2021-10-04 12:27:00 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+
+  root "welcome#home"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
